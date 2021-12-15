d7ea2a5e22b3622e45ec175027007cf7ac5c6d68
rstott87 <rstott87@yahoo.com>
2021-12-14 20:56:58 -0500

    finsished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const arrayOfMembers=[]
+function Form(props) {
+  const [member, setMember] = useState({ name: " ", role: " "});
+
+  const handleChange = event => {
+    setMember({ ...member, [event.target.name]: event.target.value });}
+  
+    const handleSubmit = event => {
+    event.preventDefault()
+    arrayOfMembers.push(member);
+    props.setMemberList(arrayOfMembers);
+  }
+
+  return(
+    <form onSubmit={event => handleSubmit(event)}>
+      <label><div>This div is in FORM {props.members.name}</div>
+        Name: 
+        <input type="text"
+                name="name" 
+                onChange={event => handleChange(event)}  />
+      </label>
+      <button>Submit!</button>
+      <label>
+        Role:
+        <input type="text" 
+              name="role"
+              onChange={event => handleChange(event)}   />
+      </label> 
+    </form>)
+}
 
 function App() {
+  const [memberList, setMemberList] = useState({name: " ", role: " "})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> Members:
+      <div>Name: {memberList.name}</div>
+      <div>Role: {memberList.role}</div>
+      </div>
+      <Form members ={memberList} setMemberList={setMemberList}/>
     </div>
   );
 }
-
 export default App;
